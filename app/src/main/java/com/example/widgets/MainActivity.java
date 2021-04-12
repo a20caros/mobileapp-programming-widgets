45d77cc9f77c7b4969f5329f823c308af0ffd023
a20caros <a20caros@student.his.se>
2021-04-12 10:45:53 +0200

    Hitta vyn edittext med hjälp av findViewByID med id edit_text ifrån activity_main.xml

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText editText = findViewById(R.id.edit_text);
     }
 }
